ba77d397566f8aca3191cf06653e4b0bf5df6720
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-24 12:40:18 +0200

    cd..

--- src/main/java/com/example/spring_thymyleaf/SimpleController.java
+++ src/main/java/com/example/spring_thymyleaf/SimpleController.java
@@ -1,13 +1,18 @@
 package com.example.spring_thymyleaf;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class SimpleController {
     @GetMapping("/")
-    public String getInfo(){
-        return "info";
+    public String getInfo(@RequestParam(value = "param1",defaultValue = "param1") String param1,
+                          @RequestParam(value = "name",defaultValue = "Przemyslaw") String name,
+                          Model model){
+        model.addAttribute("param1",param1);
+        model.addAttribute("name",name);
+        return "simplecontroller";
     }
 }
