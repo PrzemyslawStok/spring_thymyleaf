8b2b8500a59b881c6e7291e09e1f4988c233316c
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-24 11:39:13 +0200

    init

---
+++ src/test/java/com/example/spring_thymyleaf/SpringThymyleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_thymyleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymyleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
